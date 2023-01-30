c7197b93db028c1e5d75036e08f801623e304212
nathanmullins11 <ndm00002@mix.wvu.edu>
2023-01-30 12:10:04 -0500

    footer and navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TopNavBarComponent } from './Navigation/top-nav-bar/top-nav-bar.component';
+import { FooterComponent } from './Navigation/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TopNavBarComponent
+    TopNavBarComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
